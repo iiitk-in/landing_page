416aeb29cd9b7a2088c42d4bd4f801d671ffb0d9
Not A Proton <notaproton8@gmail.com>
2024-04-12 18:21:21 +0530

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,62 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>IIITK Projects Hosting Platform</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className="min-h-screen flex justify-center items-center bg-gradient-to-br from-gray-900 to-gray-800">
+        <div className="max-w-4xl">
+          <div className="h-screen flex items-center justify-center flex-col">
+            <h1 className="text-[250px] font-bold bg-gradient-to-r from-cyan-400 via-blue-800 to-green-600 text-transparent bg-clip-text bg-300% animate-gradient">
+              IIITK.in
+            </h1>
+            <h1 className="text-4xl font-bold text-center mb-6">
+              IIIT Kottayam&apos;s student-run host
+            </h1>
+            <p className="text-lg text-center mb-8">
+              Unlock Your Potential. Share Your Creativity.
+            </p>
+          </div>
+
+          <div className="grid grid-cols-1 md:grid-cols-1 gap-20">
+            <div className="bg-white rounded-xl p-8 backdrop-filter backdrop-blur-md bg-opacity-40">
+              <h2 className="text-2xl font-bold mb-4">Elevate Your Projects</h2>
+              <p>
+                Gone are the days of generic URLs. Your projects get the
+                spotlight they deserve with a sleek and personalized domain:{" "}
+                <span className="font-bold bg-gradient-to-r from-cyan-400 to-green-600 text-transparent bg-clip-text bg-300%">
+                  project-name.iiitk.in
+                </span>
+              </p>
+            </div>
+            <div className="bg-white rounded-xl p-8 backdrop-filter backdrop-blur-md bg-opacity-40">
+              <h2 className="text-2xl font-bold mb-4">
+                Powerful Backend Hosting, Simplified
+              </h2>
+              <p>
+                Focus on what truly matters - building your vision. We handle
+                the backend hosting intricacies, ensuring your projects run
+                seamlessly and securely.
+              </p>
+            </div>
+            <div className="bg-white rounded-xl p-8 mb-6 backdrop-filter backdrop-blur-md bg-opacity-40">
+              <h2 className="text-2xl font-bold mb-4">Ready?</h2>
+              <p>
+                Shoot an email at{" "}
+                <a href="mailto:admin@iiitk.in" className="text-sky-300">
+                  {" "}
+                  admin@iiitk.in
+                </a>{" "}
+                to get started.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,28 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        animatedgradient: {
+          '0%': { backgroundPosition: '0% 50%' },
+          '50%': { backgroundPosition: '100% 50%' },
+          '100%': { backgroundPosition: '0% 50%' },
+        },
+      },
+      backgroundSize: {
+        '300%': '300%',
+      },
+      animation: {
+        gradient: 'animatedgradient 10s ease infinite alternate',
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
